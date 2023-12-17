665f292728d9db3c4b88895772b336737fe7b313
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-12-17 18:51:34 +0100

    Update monstre.sh

--- monstre.sh
+++ monstre.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
 rhythmbox-client --play
-cat ansi.txt
+
+cat truck.ansi.txt
 echo "Waiting for a key to be pressed..." 
 ./noEnter
+
 rhythmbox-client --quit
 
-clear 
+LOGS=$(zenity --password --username)
 
+USERNAME=$(echo "$LOGS" | cut -d'|' -f1)
+PASSWORD=$(echo "$LOGS" | cut -d'|' -f2)
+
+
+if [ "$USERNAME" = "admin" ] && [ "$PASSWORD" = "miaou" ]; then
+	zenity --info --title="Connexion réussie" --text="Bienvenue, $USERNAME."
+else
+	zenity --error --title="Echec de la connexion" --text="Nom d'utilisateur et/ou mot de passe incorrecte."
+	exit 0
+fi
+
+clear
+
+echo "1.Instructions"
 
 exit 0
+
