1051b1b58956a04f080e27f5bf2b05853818559c
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-12-19 18:58:21 +0100

    Update monstre.sh

--- monstre.sh
+++ monstre.sh
@@ -3,12 +3,13 @@
 rhythmbox-client --play
 
 cat truck.ansi.txt
-echo "Waiting for a key to be pressed..." 
+echo "Appuyer sur une touche.." 
 ./noEnter
 
 rhythmbox-client --quit
 
-////https://ostechnix.com/zenity-create-gui-dialog-boxes-in-bash-scripts///
+sleep 1
+
 LOGS=$(zenity --password --username)
 
 USERNAME=$(echo "$LOGS" | cut -d'|' -f1)
@@ -16,15 +17,16 @@ PASSWORD=$(echo "$LOGS" | cut -d'|' -f2)
 
 
 if [ "$USERNAME" = "admin" ] && [ "$PASSWORD" = "miaou" ]; then
-	zenity --info --title="Connexion réussie" --text="Bienvenue, $USERNAME."
+	zenity --info --title="Connexion réussie" --text="Bienvenue, chef."
+	chmod 777 script.sh
 else
 	zenity --error --title="Echec de la connexion" --text="Nom d'utilisateur et/ou mot de passe incorrecte."
-	exit 0
+	
 fi
 
 clear
 
-echo "1.Instructions"
 
 exit 0
+
 
