1dd2c44ba8f4107d80d056f7170a97bcbfd897f0
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-12-26 01:30:03 +0100

    Create noEnter.c

---
+++ progc/noEnter.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+
+
+//https://winjia.medium.com/linux-getch-for-unix-c2c829721a30//
+
+int getch(void) 
+{ 
+    struct termios oldattr, newattr; 
+    int ch; 
+    tcgetattr(STDIN_FILENO, &oldattr); 
+    newattr = oldattr; 
+    newattr.c_lflag &= ~(ICANON | ECHO); 
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr); 
+    ch = getchar(); 
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr); 
+    return ch; 
+} 
+
+int main(void){
+
+	char c=getch();
+	
+	return 0;
+}
