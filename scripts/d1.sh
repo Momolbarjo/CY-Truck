76fe0afb0fd5a5c0c2add1022866e4d89cb7b8a9
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-12-27 18:33:10 +0100

    Add files via upload

---
+++ scripts/d1.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+start=$(date +%s)
+
+datafile=$1
+outputfile="temp/tmp_d1.csv"
+outputpng="temp/tmp_d1.png"
+
+
+awk -F';' 'NR>1{
+    nom_prenom = $6
+    valeur = $1
+    somme[nom_prenom ":" valeur] = 1
+} END {
+    for (i in somme) {
+        nom_prenom = substr(i, 1, index(i, ":") - 1)
+        count[nom_prenom]++
+    }
+    for (nom_prenom in count) {
+        print count[nom_prenom] " : " nom_prenom
+    }
+}' $datafile | sort -nr -k1,1 | head > "$outputfile"
+
+
+
+gnuplot <<- EOF
+    reset
+    set terminal pngcairo size 1000,900
+    set output 'temp/tmp_d1.png'
+    set datafile separator ':'
+    set style data histogram
+    set boxwidth 0.7
+    set style fill solid
+    set xtics rotate out
+    set y2tics rotate out 
+    set y2range [ 0 : 250 ]
+    set ytics nomirror
+    unset ytics # desactivation de l'axe y1 pour utiliser l'axe y2.
+    set xtics  font ",9"
+    set y2tics font ",9"
+    set y2label "NB ROUTES" font ",10"
+    set ylabel "Option -d1 : Nb routes = f(Driver)" font ",10"
+    set xlabel "DRIVER NAMES" rotate by 180 font ",10"
+    set term png  size 900,800
+    plot 'temp/tmp_d1.csv' using 1:xtic(2)  notitle with boxes axes x1y2 # graphique tracé à l'aide des axes x1 et y2.
+EOF
+
+
+convert temp/tmp_d1.png -rotate 90 images/d1.png # rotation de 90 degrés de tmp.png pour avoir un histogramme horizontale 
+xdg-open images/d1.png # ouverture du png qui contient le graphique
+
+end=$(date +%s)
+runtime=$(( $end - $start ))
+echo "Temps d'exécution : $runtime secondes"
+
+exit 0
+
+
---
+++ scripts/d2.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+start=$(date +%s)
+
+
+datafile=$1
+outputfile="temp/tmp_d2.csv"
+outputpng="temp/tmp_d2.png"
+
+LC_NUMERIC=C awk -F';' 'NR>1{
+    nom_prenom = $6
+    valeur = $5
+    if (nom_prenom in somme) {
+        somme[nom_prenom] += valeur
+    } else {
+        somme[nom_prenom] = valeur
+    }
+} END {
+    for (i in somme) {
+        printf "%.2f : %s\n", somme[i], i 
+    }
+}' $datafile | LC_NUMERIC=C sort -t':' -k1,1nr -g | head -n 10 > "$outputfile"
+
+
+gnuplot <<- EOF
+    reset
+    set terminal pngcairo size 1000,900
+    set output 'temp/tmp_d2.png'
+    set datafile separator ':'
+    set style data histogram
+    set boxwidth 0.7
+    set style fill solid
+    set xtics rotate out
+    set y2tics rotate out 
+    set y2range [ 0 : 160000 ]
+    set ytics nomirror
+    unset ytics # desactivation de l'axe y1 pour utiliser l'axe y2.
+    set xtics  font ",9"
+    set y2tics font ",9"
+    set y2label "DISTANCE (Km)" font ",10"
+    set ylabel "Option -d2 : Distance =f(Driver)" font ",10"
+    set xlabel "DRIVER NAMES" rotate by 180 font ",10"
+    set term png  size 900,800
+    plot 'temp/tmp_d2.csv' using 1:xtic(2)  notitle with boxes axes x1y2 # graphique tracé à l'aide des axes x1 et y2.
+EOF
+
+
+convert temp/tmp_d2.png -rotate 90 images/d2.png # rotation de 90 degrés de tmp.png pour avoir un histogramme horizontale 
+xdg-open images/d2.png # ouverture du png qui contient le graphique
+
+end=$(date +%s)
+runtime=$(( $end - $start ))
+echo "Temps d'exécution : $runtime secondes"
+
+exit 0
+
+
