576196c0621f73956d7ccaf55a0b96eed9d3375d
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2024-02-01 22:17:20 +0100

    Update main_s.c

--- progc/src/main_s.c
+++ progc/src/main_s.c
@@ -1,14 +1,23 @@
 #include "all.h"
 
+// main du programme C du traitement -s
 int main(void) {
 
     FILE* entree = NULL;
     FILE* sortie = NULL;
-    entree = fopen("temp/tmp_s1.csv","r");
-    pAvl racine = construireAVL(entree);
+    entree = fopen("temp/tmp_s1.csv","r"); //Ouverture du fichier d'entrée
+
+    //Verification de l'ouverture du fichier
+    if (entree == NULL){
+        fprintf(stderr, "Erreur lors de l'ouverture du fichier d'entrée");
+        exit(EXIT_FAILURE);
+    }
     
-    sortie = fopen("temp/tmp_s2.csv" , "w");
+    pAvl racine = construireAVL(entree); // Construction de l'AVL
     
+    sortie = fopen("temp/tmp_s2.csv" , "w"); //Ouverture du fichier de sortie
+
+    //Verification de l'ouverture du fichier
     if (sortie == NULL){
     	fprintf(stderr, "Erreur lors de l'ouverture du fichier de sortie");
         exit(EXIT_FAILURE);
@@ -16,9 +25,9 @@ int main(void) {
 
 
     int n = 50;
-    afficherAVLdansSortie(racine,sortie, &n);
+    afficherAVLdansSortie(racine,sortie, &n); // Affichage de 50 élément de l'AVL
 
-    
+    // Fermeture du fichier de sortie
     fclose(sortie);
     // Libérer la mémoire allouée pour l'AVL
     libererMemoireAVL(racine);
