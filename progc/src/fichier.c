1e4e398e82e301ee8e163b77b28ff7d95b95620c
Mohamed ben <119139743+Momolbarjo@users.noreply.github.com>
2024-02-01 19:09:44 +0100

    Update fichier.c

--- progc/src/fichier.c
+++ progc/src/fichier.c
@@ -39,4 +39,39 @@ void afficherAVLdansSortie(pAvl racine,FILE* sortie, int *n) {
     afficherAVLdansSortie(racine->fg,sortie, n);
 }
 
+Ville extraireDonneeCSV_2(char *ligne) {
+    	Ville ville;
+    	
+    	char nombre_elem = sscanf(ligne, "%[^,],%hu,%hu", ville.nom, &(ville.nb_fois), &(ville.nb_depart));
+    	
+    	if(nombre_elem != 3){
+        	fprintf(stderr, "Erreur, le/les types de données de votre fichier CSV ne sont pas adaptés\n");
+        	exit(EXIT_FAILURE);
+    	}
+    	
+    	return ville;
+}
+
+
+pAvl_t traitement(pAvl_t a){
+	FILE* fichier = NULL;
+	char chaine[TAILLE];
+	Ville ville;
+	fichier = fopen("temp/t1.csv","r");
+	
+	if(fichier == NULL){
+		fprintf(stderr,"Erreur lors de l'ouverture du fichier t1.csv ! \n");
+		exit(EXIT_FAILURE);
+	}
+	   
+	while(fgets(chaine, TAILLE, fichier) != NULL){
+        	ville = extraireDonneeCSV_2(chaine);
+        	a = ajoutAVL_t(a, ville);
+    	}
+    	
+    	fclose(fichier);
+	
+	return a;	
+}
+
 
