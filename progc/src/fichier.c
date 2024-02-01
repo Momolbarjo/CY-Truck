86aebfea23adb244a09f908e5cfbc82d2377ee11
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2024-02-01 22:09:10 +0100

    Update fichier.c

--- progc/src/fichier.c
+++ progc/src/fichier.c
@@ -13,6 +13,7 @@ int getch(void) {
     return ch; 
 } 
 
+// Fonction qui récupère les données d'une chaînes de caractère pour les affecter à un élément de la structure Donnee
 Donnee extraireDonneeCSV(char *ligne) {
     Donnee donnee;
     char nombre_elem = sscanf(ligne, "%u\t%f\t%f\t%f\t%f", &donnee.id, &donnee.min, &donnee.max, &donnee.max_min, &donnee.moy);
@@ -24,6 +25,7 @@ Donnee extraireDonneeCSV(char *ligne) {
     return donnee;
 }
 
+// Fonction qui affiche n éléments d'un AVL (n = nombre donnée en paramètre)
 void afficherAVLdansSortie(pAvl racine,FILE* sortie, int *n) {
     if (racine == NULL || *n == 0 || sortie == NULL) {
         return;
@@ -39,6 +41,7 @@ void afficherAVLdansSortie(pAvl racine,FILE* sortie, int *n) {
     afficherAVLdansSortie(racine->fg,sortie, n);
 }
 
+// Fonction qui récupère les données d'une chaînes de caractère pour les affecter à un élément de la structure Ville
 Ville extraireDonneeCSV_2(char *ligne) {
     	Ville ville;
     	
