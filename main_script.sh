70b94bf700b1dcdb2ccad20b94a25f8e9877be0a
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2024-02-01 22:29:06 +0100

    Update main_script.sh

--- main_script.sh
+++ main_script.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Répertoire contenant les exécutables
 executable_parent="progc/src"
 
+# Noms des exécutables
 executables=("noEnter" "main_s" "main_t")
 
-
+# Vérifier la présence des exécutables et les compiler si nécessaire
 for executable in "${executables[@]}"; do
     chemin_complet="$executable_parent/$executable"
+    
     if [ ! -f "$chemin_complet" ]; then
         echo "L'exécutable $executable n'est pas présent. Compilation en cours..."
         
@@ -21,37 +24,41 @@ for executable in "${executables[@]}"; do
     fi
 done
 
+# Vérifier si le script a les droits d'exécution
 if [ ! -x "$0" ]; then
-   echo "Vous ne disposez pas des droits , merci de vous authentifier"
+   echo "Vous ne disposez pas des droits, merci de vous authentifier"
    exit 1
 fi
 
+# Création du répertoire temporaire
 if [ ! -d "temp" ]; then
     mkdir temp
-
 else
+    # Si le répertoire existe, supprimer son contenu
     rm -rf temp/*
 fi
 
+# Création du répertoire pour les images s'il n'existe pas
 if [ ! -d "images" ]; then
     mkdir images
 fi
 
-
+# Initialiser un tableau associatif pour les options
 declare -A options
 
+# Vérifier le nombre d'arguments et le type du fichier d'entrée
 if [ "$#" -ge 2 ] && [ "$#" -lt 9 ] && [ "${1##*.}" = "csv" ]; then
 
     if [ ! -f "data/$1" ]; then
-    	echo "Erreur : le fichier doit etre present dans le dossier data."
-    	exit 1
+        echo "Erreur : le fichier doit être présent dans le dossier data."
+        exit 1
     fi
     
     datafile=$1
     
     shift
-    for opt in "$@"
-    do
+    # Traiter chaque option passée en paramètre
+    for opt in "$@"; do
         if [ "$opt" = "-h" ]; then
             echo "Usage: $0 .csv parametres (-d1,-d2,-l,-s,-t,-r)"
             exit 0
@@ -63,13 +70,14 @@ if [ "$#" -ge 2 ] && [ "$#" -lt 9 ] && [ "${1##*.}" = "csv" ]; then
         options[$opt]=true
     done
 
-    for opt in "$@"
-    do
+    # Exécuter les scripts correspondants aux options spécifiées
+    for opt in "$@"; do
         case $opt in
             -d1 | -d2 | -l | -s | -t)
                 sh "scripts/${opt#-}.sh" "data/$datafile"
                 ;;
             -r)
+                # Réduire les permissions du script
                 chmod 000 "$0"
                 ;;
             *)
